b3b095c5726691e0da82d80ee8a8f3bf854c65ed
HugoFelipe00 <hugoads2025@gmail.com>
2026-04-21 21:48:31 -0300

    HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,40 @@
+import'./homepage.css' 
+
 export function HomePage() {
-  return <main>Em construção.</main>
+  return (
+    <>
+<section className="hero">
+    <div className="hero-text">
+        <h2>CERTUS GESTÃO FINANCEIRA</h2>
+        <p>Sua solução confiável em gestão financeira</p>
+        <button>Saiba Mais</button>
+    </div>
+</section>
+
+<section className="servicos">
+    <h2>Nossos Serviços</h2>
+
+    <div className="cards">
+        <div className="card">
+            <h3>Controle de Despesas</h3>
+            <p>Gerencie suas despesas pessoais e empresariais.</p>
+        </div>
+
+        <div className="card">
+            <h3>Gestão de Contas</h3>
+            <p>Controle contas a pagar e receber com praticidade.</p>
+        </div>
+
+        <div className="card">
+            <h3>Dashboards Financeiros</h3>
+            <p>Visualize seus dados com gráficos intuitivos.</p>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <p>?</p>
+</footer>
+</>
+  )
 }
